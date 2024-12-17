af0363acd64008b018c555fd34862d27bcf5edde
Twincasper <justinwatson651@gmail.com>
2024-12-17 15:10:04 -0500

    Added spring initializr backend to root

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App.tsx';
-import UserContextProvider from './context/CurrentUserContextProvider.js';
+import UserContextProvider from './context/CurrentUserContextProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ nimbus-backend/src/test/java/com/twincasper/nimbus/nimbus_backend/NimbusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twincasper.nimbus.nimbus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimbusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
