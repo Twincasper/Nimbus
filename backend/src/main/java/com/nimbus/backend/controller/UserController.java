78aafc1ff1b29d8cd8a2266dfa6ff40e2ac395a2
Twincasper <justinwatson651@gmail.com>
2025-01-13 12:18:41 -0500

    Expanded UserService and began UserController

---
+++ backend/src/main/java/com/nimbus/backend/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.nimbus.backend.controller;
+
+import com.nimbus.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+//    Gonna need createUser, getAllUsers, getUserById, updateUser, deleteUser and checkUsernameExists
+}
--- backend/src/main/java/com/nimbus/backend/service/UserService.java
+++ backend/src/main/java/com/nimbus/backend/service/UserService.java
@@ -1,12 +1,18 @@
 package com.nimbus.backend.service;
 
 import com.nimbus.backend.model.User;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
     User createUser(String username, String password);
+
+    List<User> getAllUsers();
+    Optional<User> getUserById(Integer id);
     Optional<User> findByUsername(String username);
     boolean usernameExists(String username);
+
+    User updateUser(Integer id, User userDetails);
+
+    void deleteUser(Integer id);
 }
