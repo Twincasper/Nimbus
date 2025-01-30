73bfe4cad4088602bc2b7194267ad226a9e94212
Twincasper <justinwatson651@gmail.com>
2025-01-30 11:04:32 -0500

    Commented out delete section of profile until width is fixed

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -480,36 +480,14 @@ export default function Profile() {
                 </div>
             </div>
 
-            <div>
-                <h2 className="text-base font-semibold leading-7 text-white">Delete account</h2>
-                <p className="mt-1 text-sm leading-6 text-gray-400">
-                    No longer want to use our service? You can delete your account here. This action is not reversible.
-                    All information related to this account will be deleted permanently.
-                </p>
-            </div>
+            {/*<div>*/} This div expands the rest of the forms width
+            {/*    <h2 className="text-base font-semibold leading-7 text-white">Delete account</h2>*/}
+            {/*    <p className="mt-1 text-sm leading-6 text-gray-400">*/}
+            {/*        No longer want to use our service? You can delete your account here. This action is not reversible.*/}
+            {/*        All information related to this account will be deleted permanently.*/}
+            {/*    </p>*/}
+            {/*</div>*/}
 
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <form className="flex items-start md:col-span-2">
-                    <button
-                        type="submit"
-                        className="rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-400"
-                    >
-                        Yes, delete my account
-                    </button>
-                </form>
-            </div>
-
-            <div className="mt-6 flex items-center justify-end gap-x-6">
-                <button type="button" className="text-sm/6 font-semibold text-gray-900">
-                    Cancel
-                </button>
-                <button
-                    type="submit"
-                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-md hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                    Save
-                </button>
-            </div>
         </form>
     )
 }
