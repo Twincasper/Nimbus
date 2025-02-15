24b048b366f300d3f223ac7cc2d0b2119a7d82e8
Twincasper <justinwatson651@gmail.com>
2025-02-15 17:17:46 -0500

    Added nimbus theme and daisy ui themes to tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,28 +2,63 @@
 module.exports = {
     // darkMode: ["class"],
     content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {},
-        keyframes: {
-            move: {
-                '0%, 100%': { transform: 'translateY(-6px)' },
-                '50%': { transform: 'translateY(6px)' },
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            },
+            colors: {},
+            keyframes: {
+                move: {
+                    '0%, 100%': { transform: 'translateY(-6px)' },
+                    '50%': { transform: 'translateY(6px)' },
+                },
+            },
+            animation: {
+                move: 'move 5s ease-in-out infinite',
             },
         },
-        animation: {
-            move: 'move 5s ease-in-out infinite',
-        },
-  	},
-  },
-  plugins: [require("daisyui"), require("tailwindcss-animate"), require('@tailwindcss/forms')],
-}
-
+    },
+    plugins: [
+        require("daisyui"),
+        require("tailwindcss-animate"),
+        require('@tailwindcss/forms')
+    ],
+    daisyui: {
+        themes: [
+            // Custom theme (object)
+            {
+                nimbus: {
+                    "primary": "#FFD1DC",
+                    "secondary": "#FFE4E1",
+                    "accent": "#DB7093",
+                    "neutral": "#4A4A4A",
+                    "base-100": "#FFF0F5",
+                },
+            },
+            "light",
+            "dark",
+            "cupcake",
+            "valentine",
+            "bumblebee",
+            "emerald",
+            "corporate",
+            "synthwave",
+            "retro",
+            "cyberpunk",
+            "halloween",
+            "garden",
+            "forest",
+            "aqua",
+            "lofi",
+            "pastel",
+            "fantasy",
+            "wireframe",
+        ],
+    },
+};
